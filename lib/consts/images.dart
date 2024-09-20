c2cda7a28143b6f993ff9a1369a5924f2a585cf7
Nahin3426 <nahin3425@gmail.com>
2024-09-21 00:10:14 +0600

    Update images.dart

--- lib/consts/images.dart
+++ lib/consts/images.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 const String logo = "assets/icons/ic_logo.png";
 const ic_user = "assets/icons/ic_user.png";
 const settingsIcon = Icons.settings;
-const pokemon = Icons.add;
+
 
 var drawericonlist = <IconData>[Icons.key_rounded,Icons.people,Icons.notifications_rounded,Icons.storage_rounded,Icons.help,];
